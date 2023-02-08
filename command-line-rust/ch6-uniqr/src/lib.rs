853dc5005b7fe4532de853a4f23c54248ef8da74
Christian <christianpavilonis@outlook.com>
2023-02-08 10:27:15 -0600

    started on ch6

---
+++ command-line-rust/ch6-uniqr/src/lib.rs
@@ -0,0 +1,76 @@
+use std::{
+    error::Error,
+    fs::File,
+    io::{self, BufRead, BufReader, Read},
+};
+
+use clap::{App, Arg};
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug)]
+pub struct Config {
+    in_file: String,
+    out_file: Option<String>,
+    count: bool,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = App::new("wcr")
+        .version("0.1.0")
+        .author("Kramer")
+        .about("rust uniq")
+        .arg(
+            Arg::with_name("in_file")
+                .value_name("IN_FILE")
+                .help("input file")
+                .default_value("-"),
+        )
+        .arg(
+            Arg::with_name("out_file")
+                .value_name("OUT_FILE")
+                .help("out file"),
+        )
+        .arg(
+            Arg::with_name("count")
+                .value_name("COUNT")
+                .short("c")
+                .long("count"),
+        )
+        .get_matches();
+
+    let in_file = matches.value_of_lossy("in_file").unwrap().to_string();
+    let count = matches.is_present("count");
+
+    let out_file = matches.value_of_lossy("out_file").map(String::from);
+
+    Ok(Config {
+        in_file,
+        out_file,
+        count,
+    })
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    let mut file = open(&config.in_file).map_err(|e| format!("{}: {}", config.in_file, e))?;
+
+    let mut line = String::new();
+
+    loop {
+        let bytes = file.read_line(&mut line)?;
+        if bytes == 0 {
+            break;
+        }
+        print!("{}", line);
+        line.clear();
+    }
+
+    Ok(())
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
